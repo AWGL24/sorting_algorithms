263dbeff56676d843d6f266ede7d14126ebb933e
Drew-24 <andrewwil1@hotmail.com>
2022-02-13 13:16:27 -0400

    Fixed Code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,5 @@
 #include "sort.h"
 /**
- * swap - function that sorts two elements
- *
- * @xp: element one
- * @yp: element two
- */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-/**
  * bubble_sort - function that sorts an array of integers
  * in ascending order using the bubble sort algorithm
  *
@@ -21,13 +9,18 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int tmp = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
-				swap(&array[j], &array[j + 1]);
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
 			print_array(array, size);
 		}
 	}
