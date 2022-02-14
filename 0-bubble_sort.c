e7ccc726725b8fa618ee96f78ecddf539e1d66b0
Drew-24 <andrewwil1@hotmail.com>
2022-02-14 10:35:06 -0400

    Fixed code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int flag, tmp = 0;
-
-	if (array == NULL || size < 2)
-		return;
+	size_t i, j, temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		flag = 0;
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j + 1] < array[j])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				flag = 1;
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
-			if (flag == 0)
-				continue;
 		}
 	}
 }
